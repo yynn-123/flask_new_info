c1dd9fa863bb5dc737c8d32162da952cee836c3c
yynn-123 <55397241+yynn-123@users.noreply.github.com>
2020-06-19 07:53:39 +0800

    图标

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,7 +1,16 @@
 # 蓝图
-from flask import render_template
+from flask import render_template, current_app
 
 from . import index_blu
+
+
 @index_blu.route('/')
 def index():
     return render_template('news/index.html')
+
+
+# 浏览器在访问,在访问每个网站的时候,都会发送一个Get请求,向/favicon.ico地址获取logo
+# app中提供了方法send_static_file,会自动寻找static静态文件下面的资源
+@index_blu.route('/favicon.ico')
+def get_web_logo():
+    return current_app.send_static_file('news/favicon.ico')
