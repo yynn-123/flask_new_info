317d36963c5ff4443dd374ed2488ad4c8989a045
yynn-123 <55397241+yynn-123@users.noreply.github.com>
2020-06-22 19:05:24 +0800

    分类

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,7 +1,8 @@
 # 蓝图
 from flask import render_template, current_app, session
 
-from info.models import User
+from info import constants
+from info.models import User, News, Category
 from . import index_blu
 
 
@@ -14,15 +15,41 @@ def index():
     user_id = session.get('user_id')
     # 通过id查询用户信息
     user = None
+    news_list = None
     if user_id:
         try:
             user = User.query.get(user_id)
         except Exception as e:
             current_app.logger.errot(e)
 
-    return render_template('news/index.html',
-                           data = {'user_info':user.to_dict() if user else None})
 
+
+    try:
+        news_list = News.query.order_by(News.clicks.desc()).limit(constants.CLICK_RANK_MAX_NEWS)
+    except Exception as e:
+        current_app.logger.errot(e)
+    click_news_list = []
+    for news in news_list if news_list else []:
+        click_news_list.append(news.to_basic_dict())
+
+
+
+
+    """
+    获取新闻分类数据
+    """
+    catagories_dicts = []
+    catagories = Category.query.all()
+    for catagory in catagories:
+        catagories_dicts.append(catagory.to_dict())
+    data = {
+        'user_info': user.to_dict() if user else None,
+        'click_news_list': click_news_list,
+        'catagories':catagories_dicts
+    }
+    return render_template('news/index.html',
+                           data=data
+                           )
 
 
 # 浏览器在访问,在访问每个网站的时候,都会发送一个Get请求,向/favicon.ico地址获取logo
